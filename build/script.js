21340c3a5dc28cdd558bbc68d85462e26a8bd582
Swap <84603585+Pogo-Bash@users.noreply.github.com>
2023-07-23 19:47:49 -0500

    Add files via upload

---
+++ build/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.nav-button');
+    
+    buttons.forEach(function (button) {
+      button.addEventListener('click', function (event) {
+        event.preventDefault();
+        const url = this.getAttribute('href');
+        window.location.href = url;
+      });
+    });
+  });
