9602bb5d653ccf34846c95d3453833fb5051e515
Swap <84603585+Pogo-Bash@users.noreply.github.com>
2023-07-24 15:44:47 +0000

    api system

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const port = process.env.PORT || 3001;
 
 // Serve static files from the "public" folder
 app.use(express.static(__dirname + "/build"));
+require('dotenv').config();
 
 // Define routes for different HTML pages
 app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 app.get("/performance", (req, res) => res.sendFile(__dirname + "/build/performance.html"));
 app.get("/events", (req, res) => res.sendFile(__dirname + "/build/events.html"));
+app.get("/api/data", (req, res) => {
+  const apiKey = process.env.API_KEY;
+  // use the apiKey to fetch data from the API
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
