568ea5ee34598f9e273250677c1aafacc9b1580a
Swap <84603585+Pogo-Bash@users.noreply.github.com>
2023-07-23 19:46:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Serve static files from the "public" folder
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/build"));
 
 // Define routes for different HTML pages
-app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
-app.get("/performance", (req, res) => res.sendFile(__dirname + "/public/performance.html"));
-app.get("/photos", (req, res) => res.sendFile(__dirname + "/public/photos.html"));
-app.get("/events", (req, res) => res.sendFile(__dirname + "/public/events.html"));
-app.get("/contact", (req, res) => res.sendFile(__dirname + "/public/contact.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+app.get("/performance", (req, res) => res.sendFile(__dirname + "/build/performance.html"));
+app.get("/events", (req, res) => res.sendFile(__dirname + "/build/events.html"));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
