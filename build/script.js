c8f61d64b55bebd803597454779aec901065a429
Pogo-Bash <swapnanildasgupta9@gmail.com>
2023-07-25 01:01:12 -0500

    big css and design for index

--- build/script.js
+++ build/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.nav-button');
-    
     buttons.forEach(function (button) {
       button.addEventListener('click', function (event) {
         event.preventDefault();
